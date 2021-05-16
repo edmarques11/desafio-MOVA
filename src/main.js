4a5444a17e5fa6c1d97af3006c2bd63d05b92860
edmarques11 <edmarquessantosdelima@gmail.com>
2021-05-16 12:28:25 -0300

    add firebase for google analytics

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import FirebaseConfigs from "./firebase/config";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
@@ -11,6 +12,7 @@ Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(FirebaseConfigs);
 
 new Vue({
   router,
