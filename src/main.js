8ff38f5adc06a16df9b630b24c5b98616dd9ce7b
edmarques11 <edmarquessantosdelima@gmail.com>
2021-05-16 12:50:30 -0300

    remove firebase for google analytics

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-import FirebaseConfigs from "./firebase/config";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
@@ -12,7 +11,6 @@ Vue.config.productionTip = false;
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
-Vue.use(FirebaseConfigs);
 
 new Vue({
   router,
